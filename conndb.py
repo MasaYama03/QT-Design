b34669b7e1c8ef20af0f331cd0c512613693c772
MasaYama03 <masahiroymzk24@gmail.com>
2023-11-12 01:43:05 +0700

    first commit

---
+++ conndb.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+class conndb:
+    def __init__(self):
+        pass
+
+    def queryResult(self,strsql):
+        cnx = mysql.connector.connect(user='root', password='', host='localhost', database='mahasiswa')
+        conn = cnx.cursor()
+        conn.execute(strsql)
+        result = conn.fetchall()
+        return result
+        pass
+
+    def queryExecute(self, strsql):
+        cnx = mysql.connector.connect(user='root', password='', host='localhost', database='mahasiswa') 
+        conn = cnx.cursor()
+        conn.execute(strsql)
+        cnx.commit()
+        pass
